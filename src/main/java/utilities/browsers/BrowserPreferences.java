e03c3d6ff3991e661c1c50a64f332a342415a0f3
Dylan <dylanr66@gmail.com>
2022-07-30 09:39:01 -0400

    Addition of BrowserOptions and Encryption

---
+++ src/main/java/utilities/browsers/BrowserPreferences.java
@@ -0,0 +1,82 @@
+package utilities.browsers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import utilities.core.Hooks;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrowserPreferences {
+
+    public static void chromePrefs(ChromeOptions co) throws IOException {
+        System.setProperty("webdriver.chrome.args", "--disable-logging");
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+
+        co.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        co.setCapability("download.default_directory", System.getProperty("user.home") + "\\downloads");
+        co.setCapability("download.prompt_for_download", true);
+
+        co.addArguments("--no-sandbox", "--disable-dev-shm-usage", "enable-automation");
+
+        if (System.getProperty("Headless").equalsIgnoreCase("true")) {
+            co.addArguments("headless", "enable-automation", "window-size=1920,1080", "no-sandbox",
+                    "disable-extensions", "disable-gpu", "dns-prefetch-disable", "hide-scrollbars");
+            co.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+            enableHeadlessDownloads(co);
+        } else {
+            Hooks.driver.set(new ChromeDriver(co));
+        } // end if
+    } // end chromePrefs
+
+    public static void edgePrefs(EdgeOptions edgeOpt) {
+        System.setProperty("webdriver.edge.args", "--disable-logging");
+        System.setProperty("webdriver.edge.silentOutput", "true");
+
+        edgeOpt.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        edgeOpt.setCapability("download.default_directory", System.getProperty("user.home") + "\\downloads");
+        edgeOpt.setCapability("download.prompt_for_download", true);
+
+        if (System.getProperty("Headless").equalsIgnoreCase("true")) {
+            edgeOpt.addArguments("--headless", "enable-automation", "--window-size=1920,1080",
+                    "--no-sandbox", "--disable-extensions", "--dns-prefetch-disable");
+            edgeOpt.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        } // end if
+        Hooks.driver.set(new EdgeDriver(edgeOpt));
+    } // end edgePrefs
+
+    public static void enableHeadlessDownloads(ChromeOptions cOptions) throws IOException {
+        ChromeDriverService ds = ChromeDriverService.createDefaultService();
+        Hooks.driver.set(new ChromeDriver(ds, cOptions));
+        Map<String, Object> commandParams = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
+
+        commandParams.put("cmd", "Page.setDownloadBehavior");
+        params.put("behavior", "allow");
+        params.put("downloadPath", System.getProperty("user.home") + "\\Downloads");
+        commandParams.put("params", params);
+
+        ObjectMapper objMapper = new ObjectMapper();
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        String cmd = objMapper.writeValueAsString(commandParams);
+        String sendCmd = ds.getUrl().toString() + "/session/" + ((RemoteWebDriver) Hooks.driver.get()).getSessionId() + "/chromium/send_command";
+        HttpPost request = new HttpPost(sendCmd);
+        request.addHeader("content-type", "application/json");
+        request.setEntity(new StringEntity(cmd));
+        httpClient.execute(request);
+    }
+
+}
---
+++ src/main/java/utilities/misc/Encryption.java
@@ -0,0 +1,104 @@
+package utilities.misc;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Encryption extends JPanel implements ActionListener {
+
+    private static final String OK = "OK";
+    private final JFrame controlFrame;
+    private final JPasswordField passwordField;
+
+    public Encryption (JFrame f) {
+        controlFrame = f;
+        passwordField = new JPasswordField(10);
+        passwordField.setActionCommand(OK);
+        passwordField.addActionListener(this);
+
+        JLabel label = new JLabel("Type the password to encrypt: ");
+        label.setLabelFor(passwordField);
+        JComponent btnPane = createButtonPanel();
+
+        JPanel textPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+
+        textPane.add(label);
+        textPane.add(passwordField);
+        add(textPane);
+        add(btnPane);
+    } // end constructor
+
+    // Main method
+    public static void main(String[] args) {
+        createAndShowUI();
+    }
+
+    // Create the button panel
+    protected JComponent createButtonPanel() {
+        JPanel p = new JPanel(new GridLayout(0, 1));
+        JButton okButton = new JButton("OK");
+        okButton.setActionCommand(OK);
+        okButton.addActionListener(this);
+        return p;
+    }
+
+    // Handle the actions
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+
+        if (OK.equals(cmd)) {
+
+            StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+            encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+            encryptor.setPassword("Secret");
+            encryptor.setIvGenerator(new RandomIvGenerator());
+            encryptor.setKeyObtentionIterations(16);
+
+            char[] input = passwordField.getPassword();
+            String in = String.valueOf(input);
+
+            String encryptedPassword = encryptor.encrypt(in);
+
+            JOptionPane.showInputDialog(controlFrame, "Encrypted Password", encryptedPassword);
+            passwordField.selectAll();
+            resetFocus();
+
+        } else {
+            JOptionPane.showMessageDialog(controlFrame, "Error Occured");
+        } // end if-else
+    } // end actionPerformed
+
+    // Reset focus to text box
+    protected void resetFocus() {
+        passwordField.requestFocusInWindow();
+    } // end resetFocus
+
+    // Build the application UI and display
+    private static void createAndShowUI() {
+        JFrame frame = new JFrame("Encrypt a Password");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final Encryption newContentPane = new Encryption(frame);
+
+        // Set up with content pane
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowActivated(WindowEvent e) {
+                newContentPane.resetFocus();
+            } // end windowActivated
+        });
+
+        frame.pack();
+        frame.setVisible(true);
+    } // end createAndShowUI
+
+
+}
